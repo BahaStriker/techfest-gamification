30524093a8c3e46023f89ff32e8abb89e2b5c184
AlaEddinBouker <alaeddin.bouker@gmail.com>
2020-03-07 21:13:57 +0100

    template

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,362 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Udema a modern educational site template">
+    <meta name="author" content="Ansonika">
+    <title>SlothLab | Modern Educational plateform</title>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+    <!-- Favicons-->
+    <link rel="shortcut icon" href="{{asset('img/favicon.ico')}}" type="image/x-icon">
+    <link rel="apple-touch-icon" type="image/x-icon" href="i{{asset('mg/apple-touch-icon-57x57-precomposed.png')}}">
+    <link rel="apple-touch-icon" type="image/x-icon" sizes="72x72" href="{{asset('img/apple-touch-icon-72x72-precomposed.png')}}">
+    <link rel="apple-touch-icon" type="image/x-icon" sizes="114x114" href="{{asset('img/apple-touch-icon-114x114-precomposed.png')}}">
+    <link rel="apple-touch-icon" type="image/x-icon" sizes="144x144" href="{{asset('img/apple-touch-icon-144x144-precomposed.png')}}">
+
+    <!-- GOOGLE WEB FONT -->
+    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800" rel="stylesheet">
+
+    <!-- BASE CSS -->
+    <link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet">
+    <link href="{{asset('css/style.css')}}" rel="stylesheet">
+    <link href="{{asset('css/vendors.css')}}" rel="stylesheet">
+    <link href="{{asset('css/icon_fonts/css/all_icons.min.css')}}" rel="stylesheet">
+
+    <!-- YOUR CUSTOM CSS -->
+    <link href="{{asset('css/custom.css')}}" rel="stylesheet">
+
+</head>
+
+<body>
+
+<div id="preloader">
+    <div data-loader="circle-side"></div>
+</div>
+<!-- End Preload -->
+
+<header class="header fadeInDown">
+    <div id="logo">
+        <a href="index.html"><img src="img/logo.png" width="149" height="42" data-retina="true" alt=""></a>
+    </div>
+    <ul id="top_menu">
+        <li><a href="login.html" class="login">Login</a></li>
+        <li><a href="#0" class="search-overlay-menu-btn">Search</a></li>
+        <li>
+
+        </li>
+    </ul>
+    <!-- /top_menu -->
+</header>
+<!-- /header -->
+
+
+<main>
+    <section class="hero_single version_2">
+        <div class="wrapper">
+            <div class="container">
+                <h3>What would you learn?</h3>
+                <p>Increase your expertise in business, technology and personal development</p>
+                <form>
+                    <div id="custom-search-input">
+                        <div class="input-group">
+                            <input type="text" class=" search-query" placeholder="Ex. Architecture, Specialization...">
+                            <input type="submit" class="btn_search" value="Search">
                         </div>
-                    @endif
+                    </div>
+                </form>
+            </div>
+        </div>
+    </section>
+    <!-- /hero_single -->
 
-                    You are logged in!
+    <div class="features clearfix">
+        <div class="container">
+            <ul>
+                <li><i class="pe-7s-study"></i>
+                    <h4>+200 Games</h4><span>Explore a variety of fresh games</span>
+                </li>
+                <li><i class="pe-7s-cup"></i>
+                    <h4>Leaderboard</h4><span>Find the right instructor for you</span>
+                </li>
+                <li><i class="pe-7s-target"></i>
+                    <h4>Focus on target</h4><span>Rewards doesn't need luck it needs experience</span>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <!-- /features -->
+
+    <div class="container-fluid margin_120_0">
+        <div class="main_title_2">
+            <span><em></em></span>
+            <h2>Udema Popular Courses</h2>
+            <p>Cum doctus civibus efficiantur in imperdiet deterruisset.</p>
+        </div>
+        <div id="reccomended" class="owl-carousel owl-theme">
+            <div class="item">
+                <div class="box_grid">
+                    <figure>
+                        <a href="#0" class="wish_bt"></a>
+                        <a href="course-detail.html">
+                            <div class="preview"><span>Preview course</span></div><img src="http://via.placeholder.com/800x533/ccc/fff/course__list_1.jpg" class="img-fluid" alt=""></a>
+                        <div class="price">$39</div>
+
+                    </figure>
+                    <div class="wrapper">
+                        <small>Category</small>
+                        <h3>Persius delenit has cu</h3>
+                        <p>Id placerat tacimates definitionem sea, prima quidam vim no. Duo nobis persecuti cu.</p>
+                        <div class="rating"><i class="icon_star voted"></i><i class="icon_star voted"></i><i class="icon_star voted"></i><i class="icon_star"></i><i class="icon_star"></i> <small>(145)</small></div>
+                    </div>
+                    <ul>
+                        <li><i class="icon_clock_alt"></i> 1h 30min</li>
+                        <li><i class="icon_like"></i> 890</li>
+                        <li><a href="course-detail.html">Enroll now</a></li>
+                    </ul>
                 </div>
+            </div>
+            <!-- /item -->
+            <div class="item">
+                <div class="box_grid">
+                    <figure>
+                        <a href="#0" class="wish_bt"></a>
+                        <a href="course-detail.html"><img src="http://via.placeholder.com/800x533/ccc/fff/course__list_2.jpg" class="img-fluid" alt=""></a>
+                        <div class="price">$45</div>
+                        <div class="preview"><span>Preview course</span></div>
+                    </figure>
+                    <div class="wrapper">
+                        <small>Category</small>
+                        <h3>Persius delenit has cu</h3>
+                        <p>Id placerat tacimates definitionem sea, prima quidam vim no. Duo nobis persecuti cu.</p>
+                        <div class="rating"><i class="icon_star voted"></i><i class="icon_star voted"></i><i class="icon_star voted"></i><i class="icon_star"></i><i class="icon_star"></i> <small>(145)</small></div>
+                    </div>
+                    <ul>
+                        <li><i class="icon_clock_alt"></i> 1h 30min</li>
+                        <li><i class="icon_like"></i> 890</li>
+                        <li><a href="course-detail.html">Enroll now</a></li>
+                    </ul>
+                </div>
+            </div>
+            <!-- /item -->
+            <div class="item">
+                <div class="box_grid">
+                    <figure>
+                        <a href="#0" class="wish_bt"></a>
+                        <a href="course-detail.html"><img src="http://via.placeholder.com/800x533/ccc/fff/course__list_3.jpg" class="img-fluid" alt=""></a>
+                        <div class="price">$54</div>
+                        <div class="preview"><span>Preview course</span></div>
+                    </figure>
+                    <div class="wrapper">
+                        <small>Category</small>
+                        <h3>Persius delenit has cu</h3>
+                        <p>Id placerat tacimates definitionem sea, prima quidam vim no. Duo nobis persecuti cu.</p>
+                        <div class="rating"><i class="icon_star voted"></i><i class="icon_star voted"></i><i class="icon_star voted"></i><i class="icon_star"></i><i class="icon_star"></i> <small>(145)</small></div>
+                    </div>
+                    <ul>
+                        <li><i class="icon_clock_alt"></i> 1h 30min</li>
+                        <li><i class="icon_like"></i> 890</li>
+                        <li><a href="course-detail.html">Enroll now</a></li>
+                    </ul>
+                </div>
+            </div>
+            <!-- /item -->
+            <div class="item">
+                <div class="box_grid">
+                    <figure>
+                        <a href="#0" class="wish_bt"></a>
+                        <a href="course-detail.html"><img src="http://via.placeholder.com/800x533/ccc/fff/course__list_4.jpg" class="img-fluid" alt=""></a>
+                        <div class="price">$27</div>
+                        <div class="preview"><span>Preview course</span></div>
+                    </figure>
+                    <div class="wrapper">
+                        <small>Category</small>
+                        <h3>Persius delenit has cu</h3>
+                        <p>Id placerat tacimates definitionem sea, prima quidam vim no. Duo nobis persecuti cu.</p>
+                        <div class="rating"><i class="icon_star voted"></i><i class="icon_star voted"></i><i class="icon_star voted"></i><i class="icon_star"></i><i class="icon_star"></i> <small>(145)</small></div>
+                    </div>
+                    <ul>
+                        <li><i class="icon_clock_alt"></i> 1h 30min</li>
+                        <li><i class="icon_like"></i> 890</li>
+                        <li><a href="course-detail.html">Enroll now</a></li>
+                    </ul>
+                </div>
+            </div>
+            <!-- /item -->
+            <div class="item">
+                <div class="box_grid">
+                    <figure>
+                        <a href="#0" class="wish_bt"></a>
+                        <a href="course-detail.html"><img src="http://via.placeholder.com/800x533/ccc/fff/course__list_5.jpg" class="img-fluid" alt=""></a>
+                        <div class="price">$35</div>
+                        <div class="preview"><span>Preview course</span></div>
+                    </figure>
+                    <div class="wrapper">
+                        <small>Category</small>
+                        <h3>Persius delenit has cu</h3>
+                        <p>Id placerat tacimates definitionem sea, prima quidam vim no. Duo nobis persecuti cu.</p>
+                        <div class="rating"><i class="icon_star voted"></i><i class="icon_star voted"></i><i class="icon_star voted"></i><i class="icon_star"></i><i class="icon_star"></i> <small>(145)</small></div>
+                    </div>
+                    <ul>
+                        <li><i class="icon_clock_alt"></i> 1h 30min</li>
+                        <li><i class="icon_like"></i> 890</li>
+                        <li><a href="course-detail.html">Enroll now</a></li>
+                    </ul>
+                </div>
+            </div>
+            <!-- /item -->
+            <div class="item">
+                <div class="box_grid">
+                    <figure>
+                        <a href="#0" class="wish_bt"></a>
+                        <a href="course-detail.html"><img src="http://via.placeholder.com/800x533/ccc/fff/course__list_6.jpg" class="img-fluid" alt=""></a>
+                        <div class="price">$54</div>
+                        <div class="preview"><span>Preview course</span></div>
+                    </figure>
+                    <div class="wrapper">
+                        <small>Category</small>
+                        <h3>Persius delenit has cu</h3>
+                        <p>Id placerat tacimates definitionem sea, prima quidam vim no. Duo nobis persecuti cu.</p>
+                        <div class="rating"><i class="icon_star voted"></i><i class="icon_star voted"></i><i class="icon_star voted"></i><i class="icon_star"></i><i class="icon_star"></i> <small>(145)</small></div>
+                    </div>
+                    <ul>
+                        <li><i class="icon_clock_alt"></i> 1h 30min</li>
+                        <li><i class="icon_like"></i> 890</li>
+                        <li><a href="course-detail.html">Enroll now</a></li>
+                    </ul>
+                </div>
+            </div>
+            <!-- /item -->
+        </div>
+        <!-- /carousel -->
+        <div class="container">
+            <p class="btn_home_align"><a href="courses-grid.html" class="btn_1 rounded">View all courses</a></p>
+        </div>
+        <!-- /container -->
+        <hr>
+    </div>
+    <!-- /container -->
+
+    <div class="container margin_30_95">
+        <div class="main_title_2">
+            <span><em></em></span>
+            <h2>SlothLab Champions</h2>
+            <p>Complete your challenges and reach the top</p>
+        </div>
+        <div class="box_cart">
+            <table class="table table-striped cart-list">
+                <thead>
+                <tr>
+                    <th>
+                        Name
+                    </th>
+                    <th>
+                        Score
+                    </th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>
+                        <div class="thumb_cart">
+                            <img src="http://via.placeholder.com/150x150/ccc/fff/thumb_cart_1.jpg" alt="Image">
+                        </div>
+                        <span class="item_cart">Persius delenit has cu</span>
+                    </td>
+                    <td>
+                        <strong>69 pt</strong>
+                    </td>
+
+                </tr>
+                <tr>
+                    <td>
+                        <div class="thumb_cart">
+                            <img src="http://via.placeholder.com/150x150/ccc/fff/thumb_cart_1.jpg" alt="Image">
+                        </div>
+                        <span class="item_cart">Persius delenit has cu</span>
+                    </td>
+                    <td>
+                        <strong>69 pt</strong>
+                    </td>
+
+                </tr>
+                <tr>
+                    <td>
+                        <div class="thumb_cart">
+                            <img src="http://via.placeholder.com/150x150/ccc/fff/thumb_cart_1.jpg" alt="Image">
+                        </div>
+                        <span class="item_cart">Persius delenit has cu</span>
+                    </td>
+                    <td>
+                        <strong>69 pt</strong>
+                    </td>
+
+                </tr>
+                </tbody>
+            </table>
+            <!-- /cart-options -->
+        </div>
+        <!-- /row -->
+    </div>
+    <!-- /container -->
+
+</main>
+<!-- /main -->
+
+<footer>
+    <div class="container margin_120_95">
+        <div class="row">
+            <div class="col-lg-5 col-md-12 p-r-5">
+                <p><img src="img/logo.png" width="149" height="42" data-retina="true" alt=""></p>
+                <p>Mea nibh meis philosophia eu. Duis legimus efficiantur ea sea. Id placerat tacimates definitionem sea, prima quidam vim no. Duo nobis persecuti cu. Nihil facilisi indoctum an vix, ut delectus expetendis vis.</p>
+                <div class="follow_us">
+                    <ul>
+                        <li>Follow us</li>
+                        <li><a href="#0"><i class="ti-facebook"></i></a></li>
+                        <li><a href="#0"><i class="ti-twitter-alt"></i></a></li>
+                        <li><a href="#0"><i class="ti-google"></i></a></li>
+                        <li><a href="#0"><i class="ti-pinterest"></i></a></li>
+                        <li><a href="#0"><i class="ti-instagram"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+
+
+        </div>
+        <!--/row-->
+        <hr>
+        <div class="row">
+            <div class="col-md-8">
+                <ul id="additional_links">
+                    <li><a href="#0">Terms and conditions</a></li>
+                    <li><a href="#0">Privacy</a></li>
+                </ul>
+            </div>
+            <div class="col-md-4">
+                <div id="copy">© 2020 SlothLab</div>
             </div>
         </div>
     </div>
-</div>
-@endsection
+</footer>
+<!--/footer-->
+
+<!-- Search Menu -->
+<div class="search-overlay-menu">
+    <span class="search-overlay-close"><span class="closebt"><i class="ti-close"></i></span></span>
+    <form role="search" id="searchform" method="get">
+        <input value="" name="q" type="search" placeholder="Search..." />
+        <button type="submit"><i class="icon_search"></i>
+        </button>
+    </form>
+</div><!-- End Search Menu -->
+
+<!-- COMMON SCRIPTS -->
+<script src="{{asset('js/jquery-2.2.4.min.js')}}"></script>
+<script src="{{asset('js/common_scripts.js')}}"></script>
+<script src="{{asset('js/main.js')}}"></script>
+
+
+</body>
+</html>
