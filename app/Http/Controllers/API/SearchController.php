bbbf6c87d5aca5ee55598f30d5c5db219e083ba1
BahaStriker <bahastriker@outlook.com>
2020-03-08 01:54:29 +0100

    Search & login bg

---
+++ app/Http/Controllers/API/SearchController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\game;
+
+class SearchController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function show()
+    {
+        if ($search = request()->input('s')) {
+            $games = game::search($search)->get();
+            if(!$games->isEmpty()){
+                return response()->json($games);
+            }
+            else{
+                return response()->json(['message' => 'Sorry No Results Found!']);
+            }
+        }
+        else{
+            return response()->json(['message' => 'Invalid Action!']);
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
