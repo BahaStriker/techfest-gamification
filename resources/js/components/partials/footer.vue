3ac31db1721baad72648856ae7f3a90950482c9f
BahaStriker <bahastriker@outlook.com>
2020-03-08 02:49:19 +0100

    footer update

--- resources/js/components/partials/footer.vue
+++ resources/js/components/partials/footer.vue
@@ -4,17 +4,7 @@
             <div class="row">
                 <div class="col-lg-5 col-md-12 p-r-5">
                     <p><img src="/../img/logo.png" width="149" height="42" data-retina="true" alt=""></p>
-                    <p>Mea nibh meis philosophia eu. Duis legimus efficiantur ea sea. Id placerat tacimates definitionem sea, prima quidam vim no. Duo nobis persecuti cu. Nihil facilisi indoctum an vix, ut delectus expetendis vis.</p>
-                    <div class="follow_us">
-                        <ul>
-                            <li>Follow us</li>
-                            <li><a href="#0"><i class="ti-facebook"></i></a></li>
-                            <li><a href="#0"><i class="ti-twitter-alt"></i></a></li>
-                            <li><a href="#0"><i class="ti-google"></i></a></li>
-                            <li><a href="#0"><i class="ti-pinterest"></i></a></li>
-                            <li><a href="#0"><i class="ti-instagram"></i></a></li>
-                        </ul>
-                    </div>
+                    <p>Le site web et les services présents sur ces pages vous sont proposés par SlothLab</p>
                 </div>
 
 
@@ -23,10 +13,7 @@
             <hr>
             <div class="row">
                 <div class="col-md-8">
-                    <ul id="additional_links">
-                        <li><a href="#0">Terms and conditions</a></li>
-                        <li><a href="#0">Privacy</a></li>
-                    </ul>
+
                 </div>
                 <div class="col-md-4">
                     <div id="copy">© 2020 SlothLab</div>
