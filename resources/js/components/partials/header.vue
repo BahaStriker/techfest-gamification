2219ac74dfa9dd61f1f41a8e7a5479287a83b6ae
Amir bouker <amirboouker@gmail.com>
2020-03-08 04:31:20 +0100

    fix header

--- resources/js/components/partials/header.vue
+++ resources/js/components/partials/header.vue
@@ -1,24 +1,27 @@
 <template>
-    <header class="header fadeInDown">
-        <div id="logo">
-            <a href="index.html"><img src="img/logo.png" width="149" height="42" data-retina="true" alt=""></a>
-        </div>
-        <ul id="top_menu">
-            <li>
-                <router-link exact to="/">Home</router-link>
-            </li>
-            <li>
-                <router-link exact to="/games/list">
-                  Courses
-                </router-link>
-            </li>
-            <li>
-                <router-link exact to="/profile/1">
-                  Profile
-                </router-link>
-            </li>
-            <li><a href="login.html" class="login">Login</a></li>
-
+  <header class="header fadeInDown">
+    <div id="logo">
+      <a href="index.html">
+        <img src="img/logo.png" width="149" height="42" data-retina="true" alt />
+      </a>
+    </div>
+    <ul id="top_menu">
+      <div v-if="isLoged">
+        <li>
+          <router-link exact to="/">Home</router-link>
+        </li>
+        <li>
+          <router-link exact to="/games/list">Courses</router-link>
+        </li>
+        <li>
+          <router-link exact to="/profile/1">Profile</router-link>
+        </li>
+      </div>
+      <div v-else>
+        <li>
+          <a href="/login" class="login">Login</a>
+        </li>
+      </div>
       <li></li>
     </ul>
     <!-- /top_menu -->
@@ -29,21 +32,19 @@ export default {
   data() {
     return {
       isLoged: false,
-      userData: '',
+      userData: ""
     };
   },
-    methods: {
-        getUserData() { 
-            if(window.user)
-            {
-                this.userData = window.user;
-                this.isLoged = true;
-
-            }
-        },
-    },
-  created () {
-    this.getUserData();
+  methods: {
+    getUserData() {
+      if (window.user) {
+        this.userData = window.user;
+        this.isLoged = true;
+      }
+    }
   },
+  created() {
+    this.getUserData();
+  }
 };
 </script>
